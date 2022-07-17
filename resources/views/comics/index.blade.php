5665d1677680a21bd9f6dc7c7f949c379c1bd459
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-07-17 17:13:47 +0200

    creazione layout di index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,5 +6,28 @@
 
 @section('page-content')
     <h1>Tutti i nostri fumetti</h1>
-    @dump($comics)
+    <table class="table table-striped">
+        <thead>
+            <th scope="col">Titolo</th>
+            <th scope="col">Copertina</th>
+            <th scope="col">Prezzo</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Altro</th>
+        </thead>
+        <tbody>
+            @foreach ($comics as $comic)
+                <tr>
+                   <td>{{$comic->title}}</td>  
+                   <td><img src="{{$comic->thumb}}"></td>  
+                   <td>${{$comic->price}}</td>  
+                   <td>{{$comic->type}}</td>  
+                   <td>
+                        <a href="" class="btn btn-primary">Dettagli</a>
+                        <a href="" class="btn btn-secondary">Modifica</a>
+                        <a href="" class="btn btn-danger">Elimina</a>
+                    </td>  
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
